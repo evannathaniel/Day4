ba4bab00345371dde50786167cd38906be720297
evannathaniel <evan.nathaniel0308@gmail.com>
2022-06-16 08:30:23 +0700

    Initial Commit

---
+++ Day4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day4
+//
+//  Created by Evan N on 16/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
